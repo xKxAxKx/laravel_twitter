3c8611e00fd8a8e1535b2158d874d7837b99f4e7
xKxAxKx <pekepooooong@gmail.com>
2016-12-10 14:17:48 +0900

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('name');
+            $table->string('image')->nullable();;
+            $table->string('image_dir')->nullable();;
+            $table->text('profile')->nullable();;
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->datetime('login_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
