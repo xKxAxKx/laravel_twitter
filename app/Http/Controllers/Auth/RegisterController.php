9e0b4d98a03b07763d19381e781adda74646add5
xKxAxKx <pekepooooong@gmail.com>
2016-12-14 21:47:40 +0900

    edit RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,87 +1,87 @@
-    <?php
+<?php
 
-    namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 
-    use App\User;
-    use Validator;
-    use Image;
-    use App\Http\Controllers\Controller;
-    use Illuminate\Foundation\Auth\RegistersUsers;
+use App\User;
+use Validator;
+use Image;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
-    class RegisterController extends Controller
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        /*
-        |--------------------------------------------------------------------------
-        | Register Controller
-        |--------------------------------------------------------------------------
-        |
-        | This controller handles the registration of new users as well as their
-        | validation and creation. By default this controller uses a trait to
-        | provide this functionality without requiring any additional code.
-        |
-        */
-
-        use RegistersUsers;
-
-        /**
-         * Where to redirect users after login / registration.
-         *
-         * @var string
-         */
-        protected $redirectTo = '/home';
-
-        /**
-         * Create a new controller instance.
-         *
-         * @return void
-         */
-        public function __construct()
-        {
-            $this->middleware('guest');
-        }
-
-        /**
-         * Get a validator for an incoming registration request.
-         *
-         * @param  array  $data
-         * @return \Illuminate\Contracts\Validation\Validator
-         */
-        protected function validator(array $data){
-            return Validator::make($data, [
-                'name' => 'required|max:255',
-                'email' => 'required|email|max:255|unique:users',
-                'image' => 'image',
-                'profile' => 'max:255',
-                'password' => 'required|min:6|confirmed',
-            ]);
-        }
-
-        /**
-         * Create a new user instance after a valid registration.
-         *
-         * @param  array  $data
-         * @return User
-         */
-        protected function create(array $data){
-            // dd($data);
-
-            // getClientOriginalName()：アップロードしたファイルのオリジナル名を取得します
-            $fileName = $data['image']->getClientOriginalName();
-
-            // getRealPath()：アップロードしたファイルのパスを取得します。
-            $image = Image::make($data['image']->getRealPath());
-
-            $path = public_path() . '/images/';
-
-            // 画像を保存する
-            $image->save($path . $fileName);
-
-            return User::create([
-                'name' => $data['name'],
-                'email' => $data['email'],
-                'image' => $data['image'],
-                'profile' => $data['profile'],
-                'password' => bcrypt($data['password']),
-            ]);
-        }
+        $this->middleware('guest');
     }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data){
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            // 'image' => 'image',
+            // 'profile' => 'max:255',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data){
+        // dd($data);
+
+        // getClientOriginalName()：アップロードしたファイルのオリジナル名を取得します
+        $fileName = $data['image']->getClientOriginalName();
+
+        // getRealPath()：アップロードしたファイルのパスを取得します。
+        $image = Image::make($data['image']->getRealPath());
+
+        $path = public_path() . '/images/';
+
+        // 画像を保存する
+        $image->save($path . $fileName);
+
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'image' => $data['image'],
+            'profile' => $data['profile'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+}
